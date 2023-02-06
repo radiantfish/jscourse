3c749ea03bf924fc6f60a7882d2ba3e97b98b60b
radiantfish <radientfish@gmail.com>
2023-02-06 21:45:06 +0300

    075 Guess my number
    075 Manipulating CSS Styles

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,11 +10,48 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // when there is nop input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number! :(';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too high!';
+    } else {
+      document.querySelector('.message').textContent =
+        'You lost the game! Try again next time!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too low!';
+    } else {
+      document.querySelector('.message').textContent =
+        'You lost the game! Try again next time!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
