f03c6deed5a4851fc9aabb3cd3ac94354cd8bb2a
radiantfish <radientfish@gmail.com>
2023-02-07 21:19:41 +0300

    guess my number 077
    implementing highscore feature

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -12,6 +12,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +30,12 @@ document.querySelector('.check').addEventListener('click', function () {
     // manipulating css
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // Maniulating highscore value
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // 3) when guess is too high
   } else if (guess > secretNumber) {
